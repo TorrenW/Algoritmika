8d026c28a2d685e001d2f5fae60f20898566cafa
TorrenW <47020510+TorrenW@users.noreply.github.com>
2024-03-02 13:54:33 +1000

    Update Square.cs

--- Square.cs
+++ Square.cs
@@ -2,3 +2,14 @@
 	public Vector2 targetPosition;
 
 	public float moveStep;
+
+
+Vector2 GetRandomPoint()
+    {
+        Vector2 randomVector = new Vector2();
+ 
+        randomVector.x = Random.Range(_, _);
+        randomVector.y = Random.Range(_, _);
+ 
+        return randomVector;
+    }
