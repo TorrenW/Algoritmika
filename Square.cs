0d2970a523453c8d73bb3cc5c926c9468101d588
TorrenW <47020510+TorrenW@users.noreply.github.com>
2024-03-02 14:01:00 +1000

    Update Square.cs

--- Square.cs
+++ Square.cs
@@ -1,20 +1,25 @@
- 
-	public Vector2 targetPosition;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-	public float moveStep;
+public class Square : MonoBehaviour
+{
+    public Vector2 targetPosition;
 
-void Start () 
-	{
-		targetPosition = GetRandomPoint();
-	}
+    public float moveStep;
 
+    void Start()
+    {
+        targetPosition = GetRandomPoint();
+    }
 
-Vector2 GetRandomPoint()
+    Vector2 GetRandomPoint()
     {
         Vector2 randomVector = new Vector2();
- 
+
         randomVector.x = Random.Range(-6, 6);
         randomVector.y = Random.Range(-3, 3);
- 
+
         return randomVector;
     }
+}
