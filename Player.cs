ec10a3428f8b8c30c5642e20fa1e5cea798a7f66
TorrenW <47020510+TorrenW@users.noreply.github.com>
2024-03-09 13:43:04 +1000

    Add files via upload

--- Player.cs
+++ Player.cs
@@ -1,14 +1,43 @@
-public void TakeDamage(int damage)
-{
- health -= damage;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-      if(health > 0)
-      {
-          audioSource.PlayOneShot(damageSound);
-      }
-      else
-      {
-          int sceneIndex = SceneManager.GetActiveScene().buildIndex;
-          SceneManager.LoadScene(sceneIndex);
-      }
+public class Player : MonoBehaviour
+{
+    public int points;
+
+    public Projectile projectilePrefab;
+
+    public float shootInterval;
+    public float shootTimer;
+
+    public Transform shootPoint;
+
+    void Update()
+    {
+        Move();
+
+        shootTimer -= Time.deltaTime;
+
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        if(shootTimer <= 0)
+        {
+            Instantiate(projectilePrefab, shootPoint.position, Quaternion.identity);
+            shootTimer = shootInterval;
+        }
+    }
+
+    void Move()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 mousePos = Input.mousePosition;
+            Vector2 realPos = Camera.main.ScreenToWorldPoint(mousePos);
+            transform.position = realPos;
+        }
+    }
 }
